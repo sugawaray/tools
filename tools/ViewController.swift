ec96d1c249778b1617d3822c69be9cd89cb67acc
Yutaka Sugawara <sugawarayg@gmail.com>
2016-09-03 16:00:24 +0900

    ViewController: get a line from the textview.

--- tools/ViewController.swift
+++ tools/ViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
+   
+    // MARK: properties
+    @IBOutlet weak var txtv: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        txtv.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: UITextViewDelegate
+    func textViewDidChange(textView: UITextView) {
+        let t = txtv.text
+        let vl = t.unicodeScalars
+        if (vl.last == nil) {
+            return;
+        }
+        let nl = "\n".unicodeScalars.last!
+        if vl.last == nl {
+            let seq = vl.reverse().enumerate()
+            var t = -1
+            for (i, v) in seq {
+                if i == 0 {
+                    continue
+                } else if v == nl {
+                    t = i
+                    break;
+                }
+            }
+            if t >= 0 {
+                let subs = String(vl.suffix(t).dropLast())
+                print(subs)
+            } else {
+                print(String(vl.dropLast()))
+            }
+        }
+    }
 }
 
