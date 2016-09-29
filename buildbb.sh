a297bd4cab961994b04f65018d4c0343d180854b
Yutaka Sugawara <sugawarayg@gmail.com>
2016-09-29 22:31:52 +0900

    build: move architecture macros from xcode build settings to buildbb.sh.

--- buildbb.sh
+++ buildbb.sh
@@ -6,4 +6,11 @@
 #  Created by Yutaka Sugawara on 2016/09/29.
 #  Copyright © 2016 Yutaka Sugawara. All rights reserved.
 export ARCH="${arch}"
+archmacro=""
+case "${arch}" in
+armv7*) archmacro="__arm__";;
+arm64*) archmacro="__arm64__";;
+esac
+export CFLAGS="${CFLAGS} -arch ${arch} -D${archmacro}"
+export LDFLAGS="-arch ${arch}"
 /usr/bin/make -k V=1 "${@}"
